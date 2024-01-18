c5d3445f8aedcadd88ee91c69f2993fc00d57381
karolinybispo <karol-bobio@hotmail.com>
2024-01-18 14:27:43 -0300

    primeiro commit

---
+++ aula.js
@@ -0,0 +1,10 @@
+console.log('mostrar resultado');
+
+let vari = 23 -8;
+let ki = (34 * 2) + 89 / 2;
+
+console.log(vari , ki);
+ 
+
+
+
---
+++ desafio.js
@@ -0,0 +1,9 @@
+//RESOLVENDO DESAFIO
+
+//CALCULANDO O VALOR DA VIAGEM:
+let combustivel = 5.19;
+let gastMedio = 10;
+let dis = 1200;
+
+let resul = dis / gastMedio * combustivel;
+console.log(resul);
