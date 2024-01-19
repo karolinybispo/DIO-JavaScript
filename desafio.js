87587863b326c885dc6fc6deb414cc99fa6932d2
karolinybispo <karol-bobio@hotmail.com>
2024-01-18 21:45:17 -0300

    3 commit

--- desafio.js
+++ desafio.js
@@ -1,9 +1,20 @@
 //RESOLVENDO DESAFIO
 
 //CALCULANDO O VALOR DA VIAGEM:
-let combustivel = 5.19;
-let gastMedio = 10;
-let dis = 1200;
+let combusGaso = 5.19;
+let combusEta = 3.19;
+let gastMedio = 10; // 10km p gastar 1 litro
+let dis = 740;
 
-let resul = dis / gastMedio * combustivel;
+let carro = combusEta;
+    if(carro){
+        let resul = (dis / gastMedio) * combusEta;
 console.log(resul.toFixed(0));
+    }   
+            else {
+                let resul = (dis / gastMedio) * combusGaso;
+                console.log(resul.toFixed(0));
+    }
+
+
+//alt(option) aperte ele no texto selecionada e usando a seta p cima ou baixo eles mudam de posicao
