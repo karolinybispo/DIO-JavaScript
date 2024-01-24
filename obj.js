19955a80fad697bb1c8b90c919009be274d37c79
karolinybispo <karol-bobio@hotmail.com>
2024-01-24 20:19:10 -0300

    funcao no objeto e apenas subindo alteracao

--- obj.js
+++ obj.js
@@ -11,3 +11,23 @@ const talher = {
     console.log(talher.objtivo);
     console.log(talher);
     
+
+    //sabendo usar objeto com metodo:
+    let carro = { //objeto
+
+        marca: 'Toyota', //chave-valor (propriedade)
+ 
+        modelo: 'Corolla', 
+ 
+        ligar: function() { // funcao/metodo com nome de chave
+ 
+            console.log('O carro está ligado.'); 
+ 
+        } 
+ 
+    }; 
+ 
+   
+ 
+    carro.ligar(); // Chama o método ligar do objeto carro 
+    console.log(carro.marca);
