4477255dc47b2fdfd823d6ae06a9749db44fb848
karolinybispo <karol-bobio@hotmail.com>
2024-01-23 23:14:49 -0300

    objeto em js

---
+++ obj.js
@@ -0,0 +1,13 @@
+//criando objeto
+const talher = {
+    nome: "faca",
+    objtivo: "cortar",
+};
+        console.log(talher);
+         //adc chave valor fora do obj
+            talher.cor = "vermelha";
+    //mostrando de 3 jeitos diferentes
+    console.log(talher.nome);
+    console.log(talher.objtivo);
+    console.log(talher);
+    
