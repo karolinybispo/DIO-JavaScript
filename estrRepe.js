83afb7916138d010e8399bb96aa9d00e958285b4
karolinybispo <karol-bobio@hotmail.com>
2024-01-25 21:09:58 -0300

    for

--- estrRepe.js
+++ estrRepe.js
@@ -1,2 +1,6 @@
 // INICIANDO ESTRUTURA DE REPETICAO
-fo 
+for (let i = 0; i < 10; i++) { // for percorre uma qtd especifica de vezes. Uma variavel vai definir isso.
+ 
+    //"i < 10" essa eh uma parte booleano q defini se a condi eh verd pu false e se pode enttrar no loop
+    console.log(i);// faz o loop e mostra os numeros
+}
