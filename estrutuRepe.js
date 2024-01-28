5a42e812505e42ccbe6a32073bb73f010abba4fb
karolinybispo <karol-bobio@hotmail.com>
2024-01-27 22:00:19 -0300

    commit

--- estrutuRepe.js
+++ estrutuRepe.js
@@ -4,3 +4,24 @@ for (let i = 0; i < 10; i++) { // for percorre uma qtd especifica de vezes. Uma 
     //"i < 10" essa eh uma parte booleano q defini se a condi eh verd pu false e se pode enttrar no loop
     console.log(i);// faz o loop e mostra os numeros
 }
+
+let nome = 'meu nome eh julia';
+for (let i = 0; i < nome.length; i++){
+    console.log(nome[i]);
+}
+
+
+for (let i = 0; i <= 5; i++) {
+    console.log(i);
+}
+
+for (let j = 3; j >= 1; j--) {
+    console.log(j);
+}
+let sum = 0;
+
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+
+console.log(sum); 
